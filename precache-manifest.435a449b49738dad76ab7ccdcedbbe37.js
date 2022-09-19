d21ce85971c2fbbd9dea66af332e35373a5b8c86
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-19 13:19:59 +0300

    Updates

---
+++ precache-manifest.435a449b49738dad76ab7ccdcedbbe37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cbb9cb2d108c2a400fd5740ac2f82a",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "5507722de1c5014657c0",
+    "url": "/simple-todo/static/css/main.8eb2b0f7.chunk.css"
+  },
+  {
+    "revision": "e96ca1217e6360199ef7",
+    "url": "/simple-todo/static/js/2.069d1308.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.069d1308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5507722de1c5014657c0",
+    "url": "/simple-todo/static/js/main.4742a5ad.chunk.js"
+  },
+  {
+    "revision": "d216b762ccd8273a207c",
+    "url": "/simple-todo/static/js/runtime-main.fdb73e3f.js"
+  }
+]);
